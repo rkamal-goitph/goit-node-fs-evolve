7f66764fee0505ca72c6c2750a3a52f96bfdff14
Radhwana Kamal <r.kamal@ph.goit.global>
2024-08-29 19:34:50 +0800

    Add code for homework 01

--- contacts.js
+++ contacts.js
@@ -2,23 +2,24 @@ import fs from "fs/promises";
 import path from "path";
 import { nanoid } from "nanoid";
 
-const contactsPath = path.join("db", "contacts.json");
+const contactsPath = path.join("db", "contacts.json"); // join method automatically generates the path from the directory or folder where the contacts.json is located
 console.log(contactsPath);
 
 const listContacts = async () => {
   try {
-    const contacts = await fs.readFile(contactsPath);
-    return JSON.parse(contacts);
+    const contacts = await fs.readFile(contactsPath); // this code returns contacts in a json format
+    return JSON.parse(contacts); // we want to parse it to convert it into a JavaScript object
   } catch (error) {
     console.error("Error reading contacts:", error.message);
   }
 };
 
+// similar to the list contacts function but only returns one contact
 const getContactById = async (contactId) => {
   try {
     const contacts = await listContacts();
-    const result = contacts.find((contact) => contact.id === contactId);
-    return result || null;
+    const result = contacts.find((contact) => contact.id === contactId); // we are filtering the contact that corresponds to the contactId passed as an argument/parameter
+    return result || null; // return the filter/found value
   } catch (error) {
     console.error("Error reading contacts by id:", error.message);
   }
@@ -26,13 +27,14 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {
   try {
-    const contacts = await listContacts();
+    const contacts = await listContacts(); // returns an object
+    // works similar to the find function in the getContactById but we need the index to delete the contact
     const index = contacts.findIndex((item) => item.id === contactId);
     if (index === -1) {
       return null;
     }
-    const deletedContact = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    const deletedContact = contacts.splice(index, 1); // we are passing the index to the splice function to delete the specified contactId
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)); // we are stringifying the object before appending it back to the contacts.json
     return deletedContact;
   } catch (error) {
     console.error("Error removing contact:", error.message);
@@ -48,8 +50,8 @@ const addContact = async ({ name, email, phone }) => {
       email,
       phone,
     };
-    const allContacts = [...contacts, newContact];
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    const allContacts = [...contacts, newContact]; // spread to push the new contact to the existing contacts
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2)); // we are stringifying the object before appending it back to the contacts.json
     return newContact;
   } catch (error) {
     console.error("Error adding new contact:", error.message);
--- index.js
+++ index.js
@@ -22,12 +22,47 @@ program
   // node -v, node --version
   // this is used for checking successful installation of node
 
+  // this represents the operation that we want to run
   .option("-a, --action <type>", "choose action")
+  // represents the parameters of the CRUD functions from contacts.js
   .option("-i, --id <type>", "user id")
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: refactor
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contactsById = await getContactById(id);
+      console.log(contactsById);
+      break;
+
+    case "add":
+      const addNewContact = await addContact({ name, email, phone });
+      console.log(addNewContact);
+      break;
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
 // node index.js --action list
 // this would list all the contacts in the console
 
