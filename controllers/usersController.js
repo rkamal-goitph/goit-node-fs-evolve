debc6a0fb99ef802337803cafb7146d7c3f4d6df
Radhwana Kamal <r.kamal@ph.goit.global>
2024-09-20 21:16:08 +0800

    Resolve issue for update avatar

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,6 +9,7 @@ import {
 } from "../validation/validation.js";
 import { Jimp } from "jimp";
 import path from "path";
+import fs from "fs/promises";
 
 const { SECRET_KEY } = process.env;
 
@@ -176,7 +177,7 @@ const updateAvatar = async (req, res) => {
     await Jimp.read(oldPath)
       .then((image) => {
         console.log("Resizing image"); // Debug log
-        image.resize(250, 250).writeAsync(oldPath);
+        image.resize({ w: 250, h: 250 }).write(oldPath);
         console.log("Image resized and saved to:", oldPath); // Debug log
       })
       .catch((error) => console.log(error));
@@ -188,6 +189,11 @@ const updateAvatar = async (req, res) => {
     const extension = path.extname(originalname);
     const filename = `${_id}${extension}`;
     console.log("Generated Filename:", filename); // Debug log
+
+    // call the file system rename path function
+    const newPath = path.join("public", "avatars", filename);
+    // public/avatars/66e576387fdc812acc32be53.jpeg
+    await fs.rename(oldPath, newPath);
 
     // construct a new avatar URL
     // this may not work directly if you are using a windows OS
