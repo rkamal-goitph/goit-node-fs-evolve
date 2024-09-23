a5edac243518191b6e774ea80100777e2ecc6e98
Radhwana Kamal <r.kamal@ph.goit.global>
2024-09-23 14:38:11 +0800

    Update loginUser test

--- test/loginUser.test.js
+++ test/loginUser.test.js
@@ -1,8 +1,9 @@
-// import bcrypt from "bcryptjs";
+// import bcrypt from "bcrypt";
 // import { jest } from "@jest/globals";
 // import { User } from "../models/usersModel";
 // import request from "supertest";
 // import { app } from "../app";
+// import jwt from "jsonwebtoken";
 
 // describe("Test @POST /api/users/login", () => {
 //   // mocking a function is to make sure that the coverage of the testing is only the function that we have written as opposed to including the dependencies
@@ -11,8 +12,8 @@
 
 //   const mockSignInData = {
 //     // this is mocking the request body that we are sending to the signup and login route
-//     email: "goitsample@gmail.com",
-//     password: "goitsample116",
+//     email: "testuser05@example.com",
+//     password: "TestPassword5!",
 //   };
 
 //   // this is mocking the ID creation done by Mongodb externally
@@ -55,7 +56,7 @@
 //     jest
 //       .spyOn(User, "findByIdAndUpdate")
 //       .mockImplementation((id, fieldToBeUpdatedInTheObject) => {
-//         if (id === mockSignInData.id) {
+//         if (id === mockUserId) {
 //           return Promise.resolve({
 //             ...mockSignInData,
 //             ...fieldToBeUpdatedInTheObject,
@@ -67,7 +68,7 @@
 //   test("Login POST request with correct data", async () => {
 //     const response = await request(app)
 //       .post("/api/users/login")
-//       .send(mockUserAccount);
+//       .send(mockSignInData);
 
 //     // assertions are expected results of test suites
 //     // assertions are declared using the expect function from jest
@@ -87,85 +88,3 @@
 //     expect(user.email && user.subscription).toEqual(expect.any(String));
 //   });
 // });
-
-import request from "supertest";
-import { app } from "../app.js";
-import { User } from "../models/usersModel.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { jest } from "@jest/globals";
-
-describe("Test @POST /api/users/login", () => {
-  const signInData = {
-    email: "testuser05@example.com",
-    password: "TestPassword5!",
-  };
-
-  const mockUserId = "mockUserId";
-  const mockUser = {
-    _id: mockUserId,
-    email: signInData.email,
-    password: bcrypt.hash(signInData.password, 10),
-    subscription: "starter",
-  };
-
-  beforeAll(() => {
-    // Mock User.findOne
-    jest.spyOn(User, "findOne").mockImplementation(({ email }) => {
-      if (email === signInData.email) {
-        return Promise.resolve(mockUser);
-      }
-      return Promise.resolve(null);
-    });
-
-    // Mock bcrypt.compare
-    jest
-      .spyOn(bcrypt, "compare")
-      .mockImplementation((password, hashedPassword) => {
-        return Promise.resolve(
-          password === signInData.password &&
-            hashedPassword === mockUser.password
-        );
-      });
-
-    // Mock jwt.sign
-    jest.spyOn(jwt, "sign").mockImplementation(() => "mockJwtToken");
-
-    // Mock User.findByIdAndUpdate
-    jest.spyOn(User, "findByIdAndUpdate").mockImplementation((id, update) => {
-      if (id === mockUserId) {
-        return Promise.resolve({ ...mockUser, ...update });
-      }
-      return Promise.resolve(null);
-    });
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
-  test("Test login with correct data.", async () => {
-    const response = await request(app)
-      .post("/api/users/login")
-      .send(signInData);
-
-    console.log("Login Data:", signInData);
-    console.log("Response status code:", response.status);
-    console.log("Response body:", response.body);
-    console.log("Response body USER:", response.body.user);
-
-    // Response must have status code 200
-    expect(response.status).toBe(200);
-
-    // The token must be returned in the response
-    expect(response.body).toHaveProperty("token", "mockJwtToken");
-
-    const { user } = response.body;
-
-    // The response should return a user object with 2 fields email and subscription
-    expect(user).toHaveProperty("email" && "subscription");
-
-    // email and subscription, having the data type String
-    expect(user.email && user.subscription).toEqual(expect.any(String));
-  });
-});
